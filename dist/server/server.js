da32b1c9ca1b87ef7c2124ffbc5ff07abed87488
Chanatip Kriengkraipetch <chanatip.kr@kbtg.tech>
2018-11-19 17:13:54 +0700

    try async get redis in startGame

--- dist/server/server.js
+++ dist/server/server.js
@@ -1,13 +1,25 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const http = require("http");
 const redis = require("redis");
 const WebSocket = require("ws");
+const util_1 = require("util");
+const util_2 = require("util");
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 const redisClient = redis.createClient(6379, "127.0.0.1");
+const getAsync = util_2.promisify(redisClient.get).bind(redisClient);
+const callbacks = {};
 redisClient.on('connect', function () {
     console.log('Redis client connected');
 });
@@ -16,15 +28,62 @@ wss.on('connection', (ws) => {
     ws.on('pong', () => {
         ws.isAlive = true;
     });
-    //connection is up, let's add a simple simple event
     ws.on('message', (message) => {
         let jsonReqObj = JSON.parse(message);
-        //log the received message
-        console.log(`event: ${jsonReqObj.event}\ndata:\n${JSON.stringify(jsonReqObj.data)}\n`);
+        //console.log(`event: ${jsonReqObj.event}\ndata:\n${JSON.stringify(jsonReqObj.data)}\n`);
+        processEvent(ws, jsonReqObj.event, jsonReqObj.data);
     });
-    //send immediatly a feedback to the incoming connection    
-    ws.send('Hi there, I am a WebSocket server');
+    ws.send('BLACKJACK ONLINE');
 });
+const processEvent = function (ws, eventName, data) {
+    let registeredCallbacks = callbacks[eventName];
+    if (util_1.isNullOrUndefined(registeredCallbacks))
+        return;
+    for (let i = 0; i < registeredCallbacks.length; i++) {
+        registeredCallbacks[i](ws, data);
+    }
+};
+const isAcceptableUserName = function (name) {
+    if (util_1.isNullOrUndefined(name))
+        return false;
+    //Name cannot be empty or have spaces
+    if (name.replace(' ', '').length == 0 || name.split(' ').length > 1)
+        return false;
+    return true;
+};
+//APIs
+const startGame = function (ws, data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (!isAcceptableUserName(data.username))
+            return;
+        console.log('start game! ' + data.username);
+        redisClient.set("testkey", "haha", redis.print);
+        const result = yield getAsync('testkey');
+        console.log('async get key:' + result);
+    });
+};
+const hit = function (ws, data) {
+    if (!isAcceptableUserName(data.username))
+        return;
+    console.log('hit! ' + data.username);
+};
+const stand = function (ws, data) {
+    if (!isAcceptableUserName(data.username))
+        return;
+    console.log('stand! ' + data.username);
+};
+const leaderboard = function (ws, data) {
+    console.log('leaderboard!');
+};
+//Bindings
+const bind = function (eventName, callback) {
+    callbacks[eventName] = callbacks[eventName] || [];
+    callbacks[eventName].push(callback);
+};
+bind('startGame', startGame);
+bind('hit', hit);
+bind('stand', stand);
+bind('leaderboard', leaderboard);
 setInterval(() => {
     wss.clients.forEach((ws) => {
         if (!ws.isAlive) {
@@ -34,10 +93,10 @@ setInterval(() => {
         ws.isAlive = false;
         ws.ping();
     });
-}, 1000);
+}, 10000);
 //start our server
 server.listen(process.env.PORT || 8080, () => {
     const { port } = server.address();
-    console.log(`Server started on port ${port}`);
+    console.log('Server started on port ' + port);
 });
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -4,11 +4,15 @@ import * as net from 'net';
 import * as redis from 'redis';
 
 import * as WebSocket from 'ws';
+import { isNullOrUndefined } from 'util';
+import {promisify} from 'util';
 
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 const redisClient = redis.createClient(6379, "127.0.0.1");
+const getAsync = promisify(redisClient.get).bind(redisClient);
+const callbacks: {[key: string]: Array<Function>} = {};
 
 redisClient.on('connect', function() {
     console.log('Redis client connected');
@@ -21,18 +25,76 @@ wss.on('connection', (ws: WebSocket) => {
         ws.isAlive = true;
     });
 
-    //connection is up, let's add a simple simple event
     ws.on('message', (message: string) => {
 
         let jsonReqObj = JSON.parse(message);
 
-        //log the received message
-        console.log(`event: ${jsonReqObj.event}\ndata:\n${JSON.stringify(jsonReqObj.data)}\n`);
+        //console.log(`event: ${jsonReqObj.event}\ndata:\n${JSON.stringify(jsonReqObj.data)}\n`);
+        processEvent(ws, jsonReqObj.event, jsonReqObj.data);
     });
 
-    //send immediatly a feedback to the incoming connection    
-    ws.send('Hi there, I am a WebSocket server');
+    ws.send('BLACKJACK ONLINE');
 });
+
+const processEvent = function(ws: WebSocket, eventName: string, data: JSON){
+    let registeredCallbacks = callbacks[eventName];
+    if(isNullOrUndefined(registeredCallbacks)) 
+        return;
+
+    for(let i = 0; i < registeredCallbacks.length; i++){
+        registeredCallbacks[i](ws, data);
+    }
+};
+
+const isAcceptableUserName = function(name: string) : boolean{
+    if(isNullOrUndefined(name))
+        return false;
+    
+    //Name cannot be empty or have spaces
+    if(name.replace(' ', '').length == 0 || name.split(' ').length > 1)
+        return false;
+
+    return true;
+}
+
+//APIs
+const startGame = async function(ws: WebSocket, data: JSON){
+    if(!isAcceptableUserName(data.username))
+        return;
+
+    console.log('start game! ' + data.username);
+    redisClient.set("testkey", "haha", redis.print);
+    const result = await getAsync('testkey');
+    console.log('async get key: ' + result);
+}
+
+const hit = function(ws: WebSocket, data: JSON){
+    if(!isAcceptableUserName(data.username))
+        return;
+
+    console.log('hit! ' + data.username);
+}
+
+const stand = function(ws: WebSocket, data: JSON){
+    if(!isAcceptableUserName(data.username))
+        return;
+
+    console.log('stand! ' + data.username);
+}
+
+const leaderboard = function(ws: WebSocket, data: JSON){
+    console.log('leaderboard!');
+}
+
+//Bindings
+const bind = function(eventName: string, callback: Function){
+    callbacks[eventName] = callbacks[eventName] || [];
+    callbacks[eventName].push(callback);
+};
+bind('startGame', startGame);
+bind('hit', hit);
+bind('stand', stand);
+bind('leaderboard', leaderboard);
 
 setInterval(() => {
     wss.clients.forEach((ws: WebSocket) => {
@@ -51,5 +113,5 @@ setInterval(() => {
 //start our server
 server.listen(process.env.PORT || 8080, () => {
     const { port } = server.address() as net.AddressInfo;
-    console.log(`Server started on port ${port}`);
+    console.log('Server started on port ' + port);
 });
